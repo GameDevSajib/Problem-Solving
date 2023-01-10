ec8a32ae4729bae30965556084f252c8429f2ee6
Md Sajibur Rahman <83011864+GameDevSajib@users.noreply.github.com>
2023-01-10 09:32:28 +0600

    Add files via upload

---
+++ Integer_to_String_coversion.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define pb push_back
+
+using namespace std;
+int main()
+{
+    ll n;
+    cin>>n;
+    string s=to_string(n);
+    for(int i=0;i<s.size();i++)
+    {
+        cout<<s[i]<<endl;
+    }
+
+
+    return 0;
+}
