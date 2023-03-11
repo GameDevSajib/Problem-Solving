c7f0c3cad5a513457930cf9d39cbd52f0306f8aa
Md Sajibur Rahman <83011864+GameDevSajib@users.noreply.github.com>
2023-03-11 15:36:41 +0600

    Add files via upload

---
+++ printing_triangle.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    for(ll i=0;i<5;i++)
+    {
+        for(ll j=0;j<i;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+
+    }
+
+
+    return 0;
+}
+
+
