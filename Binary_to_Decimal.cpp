b8426a603dd371bd284cff8ba34ebfbca0c19bc9
Md Sajibur Rahman <83011864+GameDevSajib@users.noreply.github.com>
2023-04-20 11:48:08 +0600

    Add files via upload

---
+++ Binary_to_Decimal.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int decimal = 0, binary, remainder, product = 1;
+  cin >> binary;
+  while (binary != 0) {
+    remainder = binary % 10;
+    decimal = decimal + (remainder * product);
+    binary = binary / 10;
+    product *= 2;
+  }
+  cout << "The number in the decimal notation is: " << decimal ;
+  return 0;
+}
