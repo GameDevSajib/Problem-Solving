329613491ae9eb8716cff94e3903c9c78a5c1393
Md Sajibur Rahman <83011864+GameDevSajib@users.noreply.github.com>
2023-07-07 02:23:33 +0600

    Add files via upload

---
+++ cpp_class.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Sajib{
+   public:
+   
+   void sum(){
+     cout<<5+6<<endl;
+
+   }
+   
+
+};
+
+
+int main()
+{
+   Sajib s;
+   s.sum();
+  
+
+   return 0;
+}
