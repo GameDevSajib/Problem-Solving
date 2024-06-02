db4502eb17c1e398bfaa5f210f182e383396b528
Md Sajibur Rahman <83011864+GameDevSajib@users.noreply.github.com>
2024-06-02 08:29:28 +0600

    Add files via upload

---
+++ Bitwise_operator.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main()
+{
+    int a=32;
+    int b=12;int c;
+    c=a&b;
+    cout<<c<<endl;
+    c=a|b;
+    cout<<c<<endl;
+    c=a^b;
+    cout<<c<<endl;
+
+    int A=10>>2;
+    cout<<A<<endl;
+     A=10>>3;
+    cout<<A<<endl;
+    A=10<<2;
+    cout<<A<<endl;
+    A=10<<3;
+    cout<<A<<endl;
+
+
+
+    return 0;
+}
